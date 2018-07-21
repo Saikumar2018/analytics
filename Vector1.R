bb1904646b5d606c7997f741c2e4dea58a97235e
Saikumar2018 <usaikumar1994@gmail.com>
2018-07-21 18:29:11 +0530

    1

---
+++ Vector1.R
@@ -0,0 +1,21 @@
+#Data Structures - Vectors
+
+x=c(1,5,7,8,4)
+x2<-c(8,8,7,9,5)
+x
+x2
+x3=c(0,2,3)
+x3
+x4=c('M','F','M','F','M')
+X5=1:100
+X5
+(X5=1:100)
+#aSSIGN AND DISPLAY
+?SEQ
+?seq
+#For help on functions
+(x6=seq(1,100,by=3))
+(x7=seq(1,10,length.out = 5))
+marks=rnorm(60,mean = 60,sd=10)
+marks
+plot(density(marks))
